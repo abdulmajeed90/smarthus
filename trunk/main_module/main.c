75e46a7138531c5de3f68dca362e8af68b1700f8
jostorha <jostorha@637368cc-0447-11de-9a48-596ebbf2fbe4>
2009-04-20 09:57:13 +0000

    Added send and receive functions

--- trunk/main_module/main.c
+++ trunk/main_module/main.c
@@ -12,23 +12,38 @@
 
 #define BAUD 9600
 #define XBeeUART 0
-#define EthUart 1
-#define pSlave_modules 0
-#define pNumber 1
-#define pType 2
-#define pStatus 3
-#define pTemp 4
-#define pLastField 4
-#define noOfModules 3
-#define noOfBytes (1+4*noOfModules)
+#define EthUART 1
 
+#define pSlave_modules 0
+#define pHour 1 
+#define pMin 2
+#define pSec 3
+#define pNumber 4
+#define pType 5
+#define pStatus 6
+#define pTemp 7
+
+#define pModuleStart 4 //First byte that is different for each module
+#define pLastField 7
+#define pFieldsModules 4 //Number of fields in one module = pLastField-pModuleStart+1
+#define noOfModules 3
+#define noOfBytes (pModuleStart+((1+pLastField-pModuleStart)*noOfModules))
+
+typedef struct {
+	u08 type;
+	u08 status;
+	u08 temp;
+} slaveModule;
 
 FILE uart0_str = FDEV_SETUP_STREAM(uart0SendByte, NULL, _FDEV_SETUP_WRITE);
 FILE uart1_str = FDEV_SETUP_STREAM(uart1SendByte, uart1GetByte, _FDEV_SETUP_RW);
 
+slaveModule sm[3]={{0,1,5},{0,0,15},{0,1,20}};
 unsigned char ethPacket[noOfBytes];
 unsigned char ethBuffer[noOfBytes];
-void checkForethPacket(void);
+time_t time;//={21,3,7,3,1,3,9};
+void checkForEthPacket(void);
+void sendEthPacket(void);
 
 ///////////////////////////////////////////////////////////////////////////////////
 // DESCRIPTION: Main
@@ -46,7 +61,7 @@ int main(void)
 	//DDRB=0xff; //portB er utgang
 	
 	 
-    time_t time;//={21,3,7,3,1,3,9};
+    
 	uartInit();
 	uartSetBaudRate(0,BAUD);
 	uartSetBaudRate(1,BAUD);
@@ -56,8 +71,12 @@ int main(void)
 	fprintf(stderr, "Hello world!\n");
 	i2cInit();
 	i2cSetBitrate(100);	
-	if (ds1307_enable(DS1307_I2C_ADDR)) printf("ds1307 Enabled");
-	else (printf("ds1307 not found"));
+	if (ds1307_enable(DS1307_I2C_ADDR)) 
+		printf("ds1307 Enabled");
+	else 
+		printf("ds1307 not found");
+	uartFlushReceiveBuffer(EthUART);
+	uartFlushReceiveBuffer(XBeeUART);
 	//ds1307_settime(DS1307_I2C_ADDR,time);
 	unsigned char tempSec;
 	while(1)
@@ -67,30 +86,30 @@ int main(void)
 		
 		tempSec=time.sec;
 		if(ds1307_gettime(DS1307_I2C_ADDR, &time))
-		{	if(tempSec!=time.sec)	
-			//if((time.sec%10==0)&&(tempSec!=time.sec))
+		{	//if(tempSec!=time.sec)	
+			if((time.sec%10==0)&&(tempSec!=time.sec))
 			{
-				printf("\n\rTime:%d:%d:%d",time.hr,time.min,time.sec);
-				printf(ethPacket);
+				//printf("\n\rTime:%d:%d:%d",time.hr,time.min,time.sec);
+				sendEthPacket();
 			}
-			checkForethPacket();
+			//checkForEthPacket();
 		}
 		else(printf("ERROR"));
 	}
 	return 0;	
 }
-void checkForethPacket(void)
+void checkForEthPacket(void)
 {
 	wdt_disable();
-	if(!uartReceiveBufferIsEmpty(EthUart))
+	if(!uartReceiveBufferIsEmpty(EthUART))
 	{
 		int i;
 		i=0;
 		while (i<noOfBytes)
 		{
-			while (uartReceiveBufferIsEmpty(EthUart))
+			while (uartReceiveBufferIsEmpty(EthUART))
 				;;
-			uartReceiveByte(EthUart, &ethPacket[i]);
+			uartReceiveByte(EthUART, &ethPacket[i]);
 			_delay_ms(100);
 			i++;
 		}
@@ -100,10 +119,20 @@ void checkForethPacket(void)
 void sendEthPacket(void)
 {
 	ethBuffer[pSlave_modules]=noOfModules;
+	ethBuffer[pHour]=time.hr;
+	ethBuffer[pMin]=time.min;
+	ethBuffer[pSec]=time.sec;
 	int number=0;
 	while (number<noOfModules)
 	{
-		ethBuffer[pNumber+number*pLastField]=number;
-		ethBuffer[pType+number*pLastField]=number;
+		ethBuffer[pNumber+number*pFieldsModules]=number;
+		ethBuffer[pType+number*pFieldsModules]=sm[number].type;
+		ethBuffer[pStatus+number*pFieldsModules]=sm[number].status;
+		ethBuffer[pTemp+number*pFieldsModules]=sm[number].temp;		
+		number++;
+	}
+	for (int i=0;i<noOfBytes;i++)
+	{
+		uart1SendByte(ethBuffer[i]);
 	}
 }
